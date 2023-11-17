c5af224391bab64bdd9466e1336223faaa64be04
edelmanjm <edelmanjm@users.noreply.github.com>
2023-11-16 21:20:27 -0500

    Return default max_depth to 5

--- Assignment5_BoundingVolume/part1/src/renderer.ts
+++ Assignment5_BoundingVolume/part1/src/renderer.ts
@@ -63,7 +63,7 @@ export default class Renderer {
   raytracingConfig: RaytracingConfig = {
     // Antialiasing Requirement
     samples_per_pixel: 5,
-    max_depth: 1,
+    max_depth: 5,
     rand_seed: [Math.random(), Math.random(), Math.random(), Math.random()],
     weight: 0,
   };
