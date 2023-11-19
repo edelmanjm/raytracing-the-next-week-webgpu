9c24f21c4067c4d20197d81fa5b3f7a00863ec4a
edelmanjm <edelmanjm@users.noreply.github.com>
2023-11-18 22:54:50 -0500

    Fudging for AABBs seems to be working

--- Assignment5_BoundingVolume/part1/src/copyable/hittable-list.ts
+++ Assignment5_BoundingVolume/part1/src/copyable/hittable-list.ts
@@ -61,30 +61,26 @@ export class Aabb {
     let xs: number[] = m.vertices.map(v => v.px);
     let ys: number[] = m.vertices.map(v => v.py);
     let zs: number[] = m.vertices.map(v => v.pz);
-    return new Aabb(
-      [Math.min(...xs), Math.min(...ys), Math.min(...zs)],
-      [Math.max(...xs), Math.max(...ys), Math.max(...zs)],
-    );
 
-    // let minimums: number[] = [];
-    // let maximums: number[] = [];
-    //
-    // [xs, ys, zs].forEach(values => {
-    //   let min = Math.min(...values);
-    //   let max = Math.max(...values);
-    //   if (min == max) {
-    //     // Add a fudge factor
-    //     min -= 0.0001;
-    //     max += 0.0001;
-    //   }
-    //   minimums.push(min);
-    //   maximums.push(max);
-    // });
-    //
-    // return new Aabb(
-    //   [minimums[0], minimums[1], minimums[2]],
-    //   [maximums[0], maximums[1], maximums[2]],
-    // );
+    let minimums: number[] = [];
+    let maximums: number[] = [];
+
+    [xs, ys, zs].forEach(values => {
+      let min = Math.min(...values);
+      let max = Math.max(...values);
+      if (min == max) {
+        // Add a fudge factor
+        min -= 0.0001;
+        max += 0.0001;
+      }
+      minimums.push(min);
+      maximums.push(max);
+    });
+
+    return new Aabb(
+      [minimums[0], minimums[1], minimums[2]],
+      [maximums[0], maximums[1], maximums[2]],
+    );
   }
 
   static fromAabbs(box0: Aabb, box1: Aabb): Aabb {
